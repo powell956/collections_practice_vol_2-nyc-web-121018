46f4bd6647e496e0a5c06334ded31581fa7e6791
powell956 <powell956@gmail.com>
2018-12-30 20:42:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |word|
+    if !word.match(/^r/)
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.map.select.include?("a")
+end
